79820c8dbd6de38f397f23a943929fb1ffacbea6
Franco Perez <franco.perez@wolox.com.ar>
2018-02-19 13:26:30 -0300

    Removed unused imports

--- React/wbooks/src/app/index.js
+++ React/wbooks/src/app/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Home from './screens/Home/index.js';
 import './index.css';
--- React/wbooks/src/app/screens/Home/components/Book/index.js
+++ React/wbooks/src/app/screens/Home/components/Book/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import reactSvg from '../../assets/default_book.svg'
 
