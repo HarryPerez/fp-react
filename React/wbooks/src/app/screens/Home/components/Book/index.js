cc644cbb3232b4e9a333a6ead0c28d18bf92b471
Franco Perez <franco.perez@wolox.com.ar>
2018-02-22 12:08:36 -0300

    Passed to Book component the book prop only

--- React/wbooks/src/app/screens/Home/components/Book/index.js
+++ React/wbooks/src/app/screens/Home/components/Book/index.js
@@ -9,22 +9,19 @@ import styles from './styles.scss'
 const Book = props => (
   <div className={styles.bookContainer}>
     <div className={styles.bookImage}>
-      <Link to={'/books/' + props.id}>
-        <img src={props.imageUrl ? props.imageUrl : defaultBook} className={props.imageUrl ? styles.bookSvg : styles.defaultSvg} alt='bookImage' />
+      <Link to={'/books/' + props.book.id}>
+        <img src={props.book.image_url ? props.book.image_url : defaultBook} className={props.book.image_url ? styles.bookSvg : styles.defaultSvg} alt='bookImage' />
       </Link>
     </div>
     <div className={styles.bookInfo}>
-      <h1 className={styles.bookTitle}>{props.title}</h1>
-      <h2 className={styles.bookAuthor}>{props.author}</h2>
+      <h1 className={styles.bookTitle}>{props.book.title}</h1>
+      <h2 className={styles.bookAuthor}>{props.book.author}</h2>
     </div>
   </div>
 );
 
 Book.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  imageUrl: PropTypes.string,
-  id: PropTypes.number.isRequired
+  book: PropTypes.object.isRequired
 };
 
 export default Book;
