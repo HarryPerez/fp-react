951b12f4987c41d7d6451c19ec1c902c695a938c
Franco Perez <franco.perez@wolox.com.ar>
2018-03-06 10:38:26 -0300

    Removed methods from api to auth and book services.

--- React/wbooks/src/services/authService.js
+++ React/wbooks/src/services/authService.js
@@ -1,9 +1,9 @@
-import * as api from '../app/config/api';
+import api from '../app/config/api';
 
 import * as localstorageService from './localstorageService.js';
 
 export const retrieveUserData = async (name, password) => {
-  const response = await api.getUser(name, password);
+  const response = await api.post('/users/sessions', { email: name, password: password });
   if(response){
     return response.data;
   }else{
@@ -13,4 +13,14 @@ export const retrieveUserData = async (name, password) => {
 
 export const retrieveUserFromSession = () => localstorageService.retrieveUserTokenFromLocalStorage();
 
-export const registerUser = (name, password, confirmPassword, firstName, lastName) => api.saveUser(name, password, confirmPassword, firstName, lastName);
+export const registerUser = (name, password, confirmPassword, firstName, lastName) =>
+api.post('/users', {
+    user: {
+      email: name,
+      password: password,
+      confirm_password: confirmPassword,
+      first_name: firstName,
+      last_name: lastName,
+      locale: 'en'
+    }
+  });
--- React/wbooks/src/services/bookService.js
+++ React/wbooks/src/services/bookService.js
@@ -1,9 +1,9 @@
-import * as api from '../app/config/api';
+import api from '../app/config/api';
 
 import * as localStorage from './localstorageService.js';
 
 export const retrieveUserFromSession = () => localStorage.retrieveUserTokenFromLocalStorage();
 
-export const getAllBooks = () => api.getBooks();
+export const getAllBooks = () => api.get('/books');
 
-export const getBookDetail = (bookId) => api.getBookDetail(bookId);
+export const getBookDetail = (bookId) => api.get('/books/'+bookId);
