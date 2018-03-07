bc191dd0044ea232c14c972846b0ace8b06f20eb
HarryPerez <franco.perez@wolox.com.ar>
2018-03-07 12:57:43 -0300

    Changed functions to arrow functions in loader index.js

--- React/wbooks/src/app/components/Loader/index.js
+++ React/wbooks/src/app/components/Loader/index.js
@@ -4,15 +4,13 @@ import loadingGif from '../../assets/loading.gif';
 
 import styles from './styles.scss';
 
-const Loader = (Component) => {
-  return function LoadingComponent({ isLoading, ...props }) {
-    if (!isLoading) return (<Component {...props} />);
-    return (
-      <div className={styles.iconContainer}>
-        <img src={loadingGif} alt='loadingGif'/>
-      </div>
-    );
-  }
+const Loader = (Component) => ({ isLoading, ...props }) => {
+  if (!isLoading) return (<Component {...props} />);
+  return (
+    <div className={styles.iconContainer}>
+      <img src={loadingGif} alt='loadingGif'/>
+    </div>
+  );
 }
 
 export default Loader;
